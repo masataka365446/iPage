9d10a439eaa64d88a31e6eb16265d0fbe1cd8ecd
masataka365446 <masataka3596@gmail.com>
2023-01-29 16:41:57 +0900

    first commit

---
+++ Pods/Target Support Files/Pods-WebLinkApp/Pods-WebLinkApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WebLinkApp : NSObject
+@end
+@implementation PodsDummy_Pods_WebLinkApp
+@end
---
+++ WebLinkApp/Controller/TabBarIconCollectionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TabBarIconCollectionViewController.swift
+//  WebLinkApp
+//
+//  Created by 福原雅隆 on 2023/01/28.
+//
+
+import UIKit
+
+//private let reuseIdentifier = "Cell"
+
+class TabBarIconCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func button_01(_ sender: Any) {
+        print("buttton_01が押されました")
+    }
+    
+
+
+}
